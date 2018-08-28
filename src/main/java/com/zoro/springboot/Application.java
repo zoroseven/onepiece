0635f4f3c521beb3596146316d96e46f7bb949bd
gaocheng <18642659280@163.com>
2018-08-28 21:59:00 +0800

    去掉热部署插件，影响redisAppender初始化，会被两个加载器加载

--- src/main/java/com/zoro/springboot/Application.java
+++ src/main/java/com/zoro/springboot/Application.java
@@ -2,7 +2,6 @@ package com.zoro.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 @SpringBootApplication
@@ -10,5 +9,16 @@ public class Application {
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
+        //排除指定加载器
+        //SpringApplicationBuilder builder =  new SpringApplicationBuilder(Application.class) ;
+        //Set<ApplicationListener<?>> listeners = builder.application().getListeners() ;
+        //for (Iterator<ApplicationListener<?>> it = listeners.iterator(); it.hasNext() ;) {
+        //    ApplicationListener<?> listener = it.next() ;
+        //    if (listener  instanceof RestartApplicationListener) {
+        //        it.remove() ;
+        //    }
+        //}
+        //builder.application().setListeners(listeners) ;
+        //builder.run(args) ;
     }
 }
