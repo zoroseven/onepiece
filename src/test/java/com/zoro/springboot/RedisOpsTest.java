a9ed9e7139b2a6287305c3fe6f690e69c90c501a
zoroseven <36258063+zoroseven@users.noreply.github.com>
2018-09-01 09:01:41 +0800

    增加junit测试

---
+++ src/test/java/com/zoro/springboot/RedisOpsTest.java
@@ -0,0 +1,33 @@
+package com.zoro.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.Set;
+
+/**
+ * @author gaocheng
+ * @date 2018/8/30  22:24
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class RedisOpsTest {
+
+	@Resource(name = "redisTemplate")
+	private RedisTemplate redisTemplate;
+
+	@Test
+	public void setOpsTest(){
+//		String[] strarrays = new String[]{"strarr3","sgtarr2"};
+//		System.out.println(redisTemplate.opsForSet().add("set",strarrays));
+//		Set<String> set = redisTemplate.opsForSet().members("set");
+//		System.out.println(set.contains("strarr1"));
+//		System.out.println(set.contains("strarr"));
+		System.out.println(redisTemplate.opsForSet().isMember("set","strarr1"));//true
+		System.out.println(redisTemplate.opsForSet().isMember("set","strarr"));//false
+	}
+}
